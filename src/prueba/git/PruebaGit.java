9e8d343e43e2f137d976380c12dd891ce50124c4
vina <vina@VIAWSE02LC0220.duoc.acad>
2024-08-28 14:40:18 -0400

    probando git

---
+++ src/prueba/git/PruebaGit.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba.git;
+
+/**
+ *
+ * @author vina
+ */
+public class Auto {
+
+       private String marca,modelo,tipoTrasnmision,tipoCombustible;
+       private int año,cantidadPuerta;
+       private double cilindrada;
+
+}
